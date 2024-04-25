e795d87af3acd2253429201aa292556e434e432b
Yohan Berga <yohan520@gmail.com>
2024-04-25 01:16:43 -0400

    Velocity Glyph Colors and Clipping

--- main.py
+++ main.py
@@ -29,6 +29,8 @@ if __name__=="__main__":
                                                             "expansivity", "velocity"], default="temperature", 
                                                             help = "Which field to visualize (default: temperature)")
     parser.add_argument("-r", "--resolution", type=int, default=100, help="Resolution of dataset voxelization")
+    parser.add_argument("--color_by_vector", action='store_true', default=False, 
+                        help="Colors velocity glyphs by vector orientation instead of by magnitude.")
     parser.add_argument("--camera", type=str, default='camera.json',
                         help="camera json")
     args = parser.parse_args()
@@ -48,7 +50,8 @@ if __name__=="__main__":
     elif args.field == "expansivity":
         window = PyQtExpansivity(data, resolution=args.resolution)
     elif args.field == "velocity":
-        window = PyQtVelocity(data, resolution=args.resolution)
+        color_mode = "magnitude" if args.color_by_vector is True else "orientation"
+        window = PyQtVelocity(data, resolution=args.resolution, color_mode=color_mode)
     else:
         raise argparse.ArgumentTypeError('Field has to be temperature, conductivity, anomaly, expansivity, or velocity!')
     # window.load_camera(args.camera)
--- mantle/velocity_field.py
+++ mantle/velocity_field.py
@@ -13,14 +13,14 @@ class PyQtVelocity(PyQtBase):
     def __init__(self, 
         data,
         output='velocity',
-        resolution = 100
+        resolution = 100,
+        color_mode = "magnitude"
     ):
         super().__init__(output)
         self.ui = UiBase()
         self.ui.setupUi(self)
 
         # temperature = np.array(data[0].temperature)
-        # Errmmm.. Needs change
         self.tf = TransferFunction(
             ctf_tuples=[
                 [0, 0.0, 1.0, 0.0],
@@ -36,12 +36,14 @@ class PyQtVelocity(PyQtBase):
             title="Velocity"
         ) 
 
+        self.color_mode = color_mode
         self.resolution = resolution
-        self.data = data[0]
+        self.time_idx = 0
+        self.data = data
 
-        self.vx_image_data = voxelize(self.data, 'vx', resolution=self.resolution, clip_theta1=90)
-        self.vy_image_data = voxelize(self.data, 'vy', resolution=self.resolution, clip_theta1=90)
-        self.vz_image_data = voxelize(self.data, 'vz', resolution=self.resolution, clip_theta1=90)
+        self.vx_image_data = voxelize(self.data[self.time_idx], 'vx', resolution=self.resolution, clip_theta1=90)
+        self.vy_image_data = voxelize(self.data[self.time_idx], 'vy', resolution=self.resolution, clip_theta1=90)
+        self.vz_image_data = voxelize(self.data[self.time_idx], 'vz', resolution=self.resolution, clip_theta1=90)
 
 
         vector_field = vtk.vtkImageData()
@@ -49,26 +51,6 @@ class PyQtVelocity(PyQtBase):
         vector_field.GetPointData().SetScalars(self.vx_image_data.GetPointData().GetScalars())
         vector_field.GetPointData().AddArray(self.vy_image_data.GetPointData().GetScalars())
         vector_field.GetPointData().AddArray(self.vz_image_data.GetPointData().GetScalars())
-
-        def print_vector_field(vector_field):
-            # Get dimensions of the vector field
-            dims = vector_field.GetDimensions()
-
-            # Get scalar values
-            scalar_array = vector_field.GetPointData().GetScalars()
-
-            # Loop through points and print scalar values
-            for k in range(dims[2]):
-                for j in range(dims[1]):
-                    for i in range(dims[0]):
-                        index = i + dims[0] * (j + dims[1] * k)
-                        scalar_value = scalar_array.GetValue(index)
-                        if scalar_value != -10000:
-                            print(f"Point ({i}, {j}, {k}): Scalar Value = {scalar_value}")
-
-        # vector field super scuff rn like only 3 dimensions with scalars??? 
-        # nahhhh that shit needs to be 3 dimensions with vectors of 3 dimensions
-
 
         num_points = self.vx_image_data.GetNumberOfPoints()
 
@@ -90,6 +72,19 @@ class PyQtVelocity(PyQtBase):
         threshold.SetLowerThreshold(-9999)  # Set threshold to filter out -10000
         threshold.Update()
         
+        # Get the scalar range
+        scalarRange = threshold.GetOutput().GetScalarRange()
+        minScalarValue = scalarRange[0]
+        maxScalarValue = scalarRange[1]
+
+        ctf = vtk.vtkColorTransferFunction()
+        # ctf.AddRGBPoint(minScalarValue, 0.001462, 0.000466, 0.013866)
+        # ctf.AddRGBPoint((minScalarValue + maxScalarValue) / 2, 0.365953, 0.146965, 0.472712)
+        # ctf.AddRGBPoint(maxScalarValue, 0.988362, 0.998364, 0.644924)
+        ctf.AddRGBPoint(minScalarValue, 0, 0, 1)
+        # ctf.AddRGBPoint((minScalarValue + maxScalarValue) / 2, 0, 1, 0)
+        ctf.AddRGBPoint(maxScalarValue, 1, 0, 0)
+
         # Create glyphs to visualize the vector field
         # Create arrow source for glyphs
         arrow_source = vtk.vtkArrowSource()
@@ -98,33 +93,31 @@ class PyQtVelocity(PyQtBase):
         # Set the arrow source as the glyph source
         glyph.SetSourceConnection(arrow_source.GetOutputPort())
         glyph.SetInputConnection(threshold.GetOutputPort())
-        glyph.SetVectorModeToUseVector()
-        # glyph.SetScaleModeToScaleByScalar()
-        # glyph.OrientOn()
-        glyph.SetColorModeToColorByVector()
         glyph.SetScaleFactor(1e9)  # Adjust the scaling factor as needed
+        if self.color_mode == "magnitude":
+            glyph.SetColorModeToColorByScale()
+        else:
+            glyph.SetColorModeToColorByVector()
         # glyph.SetScaleFactor(1.5e-6)
-
-
-
 
         # Create mapper and actor for glyphs
         glyph_mapper = vtk.vtkPolyDataMapper()
         glyph_mapper.SetInputConnection(glyph.GetOutputPort())
+        glyph_mapper.SetLookupTable(ctf)
+        glyph_mapper.SetScalarRange(minScalarValue, maxScalarValue)
 
-        glyph_actor = vtk.vtkActor()
-        glyph_actor.SetMapper(glyph_mapper)
-
-        
+        self.glyph_actor = vtk.vtkActor()
+        self.glyph_actor.SetMapper(glyph_mapper)
 
         # Create the Renderer
         self.ren = vtk.vtkRenderer()
+        self.first_render = True
         # self.ren.AddVolume(self.field_temperature.volume)
-        self.ren.AddActor(glyph_actor)
+        self.ren.AddActor(self.glyph_actor)
         # self.ren.AddActor(self.field_temperature.actor)
         # self.ren.AddActor(isosurface.actor)
         # self.ren.AddActor(self.axes.actor)
-        self.ren.AddActor2D(self.tf.bar.get())
+        # self.ren.AddActor2D(self.tf.bar.get())
         self.ren.ResetCamera()
         self.ren.GradientBackgroundOn()  # Set gradient for background
         self.ren.SetBackground(0.0, 0.0, 0.0)  # Set background to silver
@@ -132,16 +125,89 @@ class PyQtVelocity(PyQtBase):
 
         self.ui.vtkWidget.GetRenderWindow().AddRenderer(self.ren)
         self.iren = self.ui.vtkWidget.GetRenderWindow().GetInteractor()
+        self.first_render = False
 
         self.Update()
         self.set_callback()
 
     def update_clipper(self):
-        self.vtk_image_data = voxelize(self.data, 'temperature', resolution=self.resolution, 
-                                       clip_theta1=self.clipX, clip_theta2=self.clipY)
+        
+        if not self.first_render:
+            self.ren.RemoveActor(self.glyph_actor)
 
-        self.ren.RemoveVolume(self.field_temperature.volume)
+        self.vx_image_data = voxelize(
+            self.data[self.time_idx], 'vx', resolution=self.resolution, 
+            clip_theta1=self.clipX, clip_theta2=self.clipY)
+        self.vy_image_data = voxelize(
+            self.data[self.time_idx], 'vy', resolution=self.resolution, 
+            clip_theta1=self.clipX, clip_theta2=self.clipY)
+        self.vz_image_data = voxelize(
+            self.data[self.time_idx], 'vz', resolution=self.resolution, 
+            clip_theta1=self.clipX, clip_theta2=self.clipY)
 
-        self.field_temperature = ScalarField(self.vtk_image_data, self.tf)
+        # TODO:
+        # All of this below is just repeated code from before. 
+        # It's late and I don't want to make a better solution for now right before
+        # presentations, so here we go.
 
-        self.ren.AddVolume(self.field_temperature.volume)
+        vector_field = vtk.vtkImageData()
+        vector_field.DeepCopy(self.vx_image_data) 
+        vector_field.GetPointData().SetScalars(self.vx_image_data.GetPointData().GetScalars())
+        vector_field.GetPointData().AddArray(self.vy_image_data.GetPointData().GetScalars())
+        vector_field.GetPointData().AddArray(self.vz_image_data.GetPointData().GetScalars())
+
+        num_points = self.vx_image_data.GetNumberOfPoints()
+
+        vectors = vtk.vtkFloatArray()
+        vectors.SetNumberOfComponents(3)
+        vectors.SetNumberOfTuples(num_points)
+
+        for i in range(num_points):
+            x = self.vx_image_data.GetPointData().GetScalars().GetValue(i)
+            y = self.vy_image_data.GetPointData().GetScalars().GetValue(i)
+            z = self.vz_image_data.GetPointData().GetScalars().GetValue(i)
+            vectors.SetTuple3(i, x, y, z)
+
+        vector_field.GetPointData().SetVectors(vectors)
+
+        # Filter out points with scalar value equal to -10000
+        threshold = vtk.vtkThreshold()
+        threshold.SetInputData(vector_field)
+        threshold.SetLowerThreshold(-9999)  # Set threshold to filter out -10000
+        threshold.Update()
+        
+        # Get the scalar range
+        scalarRange = threshold.GetOutput().GetScalarRange()
+        minScalarValue = scalarRange[0]
+        maxScalarValue = scalarRange[1]
+
+        ctf = vtk.vtkColorTransferFunction()
+        ctf.AddRGBPoint(minScalarValue, 0.001462, 0.000466, 0.013866)
+        ctf.AddRGBPoint((minScalarValue + maxScalarValue) / 2, 0.365953, 0.146965, 0.472712)
+        ctf.AddRGBPoint(maxScalarValue, 0.988362, 0.998364, 0.644924)
+
+        # Create glyphs to visualize the vector field
+        # Create arrow source for glyphs
+        arrow_source = vtk.vtkArrowSource()
+
+        glyph = vtk.vtkGlyph3D()
+        # Set the arrow source as the glyph source
+        glyph.SetSourceConnection(arrow_source.GetOutputPort())
+        glyph.SetInputConnection(threshold.GetOutputPort())
+        glyph.SetScaleFactor(1e9)  # Adjust the scaling factor as needed
+        if self.color_mode == "magnitude":
+            glyph.SetColorModeToColorByScale()
+        else:
+            glyph.SetColorModeToColorByVector()
+        # glyph.SetScaleFactor(1.5e-6)
+
+        # Create mapper and actor for glyphs
+        glyph_mapper = vtk.vtkPolyDataMapper()
+        glyph_mapper.SetInputConnection(glyph.GetOutputPort())
+        glyph_mapper.SetLookupTable(ctf)
+        glyph_mapper.SetScalarRange(minScalarValue, maxScalarValue)
+
+        self.glyph_actor = vtk.vtkActor()
+        self.glyph_actor.SetMapper(glyph_mapper)
+        
+        self.ren.AddActor(self.glyph_actor)
